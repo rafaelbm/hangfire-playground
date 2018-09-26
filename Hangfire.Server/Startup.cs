adfb018fe05e7ebca729293d35481b82d2ae6577
rafaelbm <5865557+rafaelbm@users.noreply.github.com>
2018-09-26 19:27:15 -0300

    Add project files.

---
+++ Hangfire.Server/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Hangfire;
+using Hangfire.Console;
+using Hangfire.Dashboard;
+using Hangfire.Server;
+using Microsoft.Owin;
+using Owin;
+using System;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace Hangfire.Server
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalConfiguration.Configuration.UseSqlServerStorage("HangFire")
+                .UseDashboardMetric(DashboardMetrics.ServerCount)
+                .UseDashboardMetric(DashboardMetrics.RecurringJobCount)
+                .UseDashboardMetric(DashboardMetrics.RetriesCount)
+                .UseDashboardMetric(DashboardMetrics.EnqueuedCountOrNull)
+                .UseDashboardMetric(DashboardMetrics.FailedCountOrNull)
+                .UseDashboardMetric(DashboardMetrics.EnqueuedAndQueueCount)
+                .UseDashboardMetric(DashboardMetrics.ScheduledCount)
+                .UseDashboardMetric(DashboardMetrics.ProcessingCount)
+                .UseDashboardMetric(DashboardMetrics.SucceededCount)
+                .UseDashboardMetric(DashboardMetrics.FailedCount)
+                .UseDashboardMetric(DashboardMetrics.DeletedCount)
+                .UseDashboardMetric(DashboardMetrics.AwaitingCount)
+                .UseConsole();
+
+            app.UseHangfireDashboard("/hangfire");
+
+            var options = new BackgroundJobServerOptions()
+            {
+                WorkerCount = 10
+            };
+
+            app.UseHangfireServer(options);
+        }
+    }
+}
